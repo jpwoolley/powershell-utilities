000f0695aec2501731ea887165c63606d7a09463
Jonathan Woolley <jonathan@jpwoolley.co.uk>
2022-07-22 09:26:41 +0100

    corrected spelling on Test-IsTelephoneNumberAvailable.ps1

--- microsoft-online-services/teams/Test-IsTelephoneNumberAvailable.ps1
+++ microsoft-online-services/teams/Test-IsTelephoneNumberAvailable.ps1
@@ -1,11 +1,11 @@
-function Test-IsTelephoneNumberIsAvailable ($number) {
+function Test-IsTelephoneNumberAvailable ($number) {
   <#
 .SYNOPSIS
   Tests where a telephone number is already assigned in Microsoft Teams. Returns true (if the number is available) or false (if it's already in use)
 .DESCRIPTION
   Tests where a telephone number is already assigned in Microsoft Teams. Returns true (if the number is available) or false (if it's already in use)
 .EXAMPLE
-  Test-IsTelephoneNumberIsAvailable -number +442079460621
+  Test-IsTelephoneNumberAvailable -number +442079460621
 #>
 
   $test = Get-CsOnlineUser | Where-Object { $_.LineURI -Like "*$($number)" -or $_.PrivateLine -Like "*$($number)" }
